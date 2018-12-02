22b1f545e840e6945e63cc52ca379dad15d0e40b
audrey <audrey.guilloud2201@gmail.com>
2018-12-02 21:24:50 +0100

    favicon

--- src/AppBundle/Service/MarvelApi.php
+++ src/AppBundle/Service/MarvelApi.php
@@ -18,29 +18,30 @@ class MarvelApi{
         $this->container = $container;
     }
 
-
+    /**
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
     public function fetchAllCharacters(){
-
         $this->privateKey = $this->container->getParameter('private.key');
         $this->publicKey = $this->container->getParameter('public.key');
         $cache = new FilesystemCache();
         $data = [];
 
-
         $ts = time();
         $hash = md5($ts . $this->privateKey . $this->publicKey);
 
-
+        //if the response of the api is not cached yet
         if (!$cache->has('heroes')) {
             $client = new Client();
             $res = $client->request('GET', 'http://gateway.marvel.com/v1/public/characters?offset=99&ts=' . $ts . '&apikey=' . $this->publicKey . '&hash=' . $hash);
             $response = json_decode($res->getBody(), true);
             $cache->set('heroes', $response);
         } else {
+            // if it's cached, we retrieve the cache item
             $response = $cache->get('heroes');
         }
-
-
 
         foreach ($response['data']['results'] as $heros){
             $data[] = ['thumbnail' => $heros['thumbnail']['path'].'.'.$heros['thumbnail']['extension'], 'name' => $heros['name']];
@@ -49,22 +50,32 @@ class MarvelApi{
         return $data;
     }
 
+    /**
+     * @param $name
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
     public function fetchOneCharacter($name){
         $this->privateKey = $this->container->getParameter('private.key');
         $this->publicKey = $this->container->getParameter('public.key');
         $cache = new FilesystemCache();
+
+        //clean the name of the hero without reserved characters
         $reservedCharacters = ['(', ')'];
         $nameCleaned = $key = str_replace($reservedCharacters, '', $name);
 
         $ts = time();
         $hash = md5($ts . $this->privateKey . $this->publicKey);
 
+        //if the response of the api is not cached yet
         if (!$cache->has('hero'.$nameCleaned)) {
             $client = new Client();
             $res = $client->request('GET', 'http://gateway.marvel.com/v1/public/characters?name=' . $name . '&ts=' . $ts . '&apikey=' . $this->publicKey . '&hash=' . $hash);
             $response = json_decode($res->getBody(), true);
             $cache->set('hero' . $nameCleaned, $response);
         }else{
+            //we retrieve the cache item
             $response = $cache->get('hero' . $nameCleaned);
         }
 
